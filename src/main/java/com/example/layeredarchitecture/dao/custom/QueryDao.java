e3f0c7b01274b19da9e728fae07b75a306c009be
LRLayan <rameshlayan4@gmail.com>
2023-12-22 14:11:01 +0530

    Loose coupling QueryDaoImpl

--- src/main/java/com/example/layeredarchitecture/dao/custom/QueryDao.java
+++ src/main/java/com/example/layeredarchitecture/dao/custom/QueryDao.java
@@ -1,8 +1,12 @@
 package com.example.layeredarchitecture.dao.custom;
 
-import com.example.layeredarchitecture.dao.JoinQueryDao;
+import com.example.layeredarchitecture.dao.SuperDao;
+
+import java.sql.SQLException;
+import java.util.List;
 
 
-public interface QueryDao extends JoinQueryDao {
+public interface QueryDao<T> extends SuperDao {
+    List<T> viewOrderDetail() throws SQLException, ClassNotFoundException;
 
 }
