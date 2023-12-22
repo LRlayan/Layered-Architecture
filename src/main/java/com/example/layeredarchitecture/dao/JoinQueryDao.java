c76d021893ed1caf2430b2e5fc9aec5f8a0a623d
LRLayan <rameshlayan4@gmail.com>
2023-12-22 14:08:11 +0530

    Create Super type interface

--- src/main/java/com/example/layeredarchitecture/dao/JoinQueryDao.java
+++
@@ -1,9 +0,0 @@
-package com.example.layeredarchitecture.dao;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
-
-public interface JoinQueryDao<T> {
-    List<T> viewOrderDetail() throws SQLException, ClassNotFoundException;
-}
---
+++ src/main/java/com/example/layeredarchitecture/dao/SuperDao.java
@@ -0,0 +1,8 @@
+package com.example.layeredarchitecture.dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface SuperDao<T> {
+
+}
